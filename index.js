cd6b8d18ff699999339af6f71d5f35466163a189
r48patel <r48patel@gmail.com>
2017-02-06 23:16:37 -0600

    Changing the tile generation with for loop along with text file

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 var express = require('express');
 var app = express();
+var fs = require('fs');
+
+var gallery_info=[];
+var projects_info = fs.readFileSync(__dirname + '/views/templates/pics.txt').toString().split('\n');
+
+for(var i=0; i<projects_info.length;i+=3){
+	gallery_info.push({
+		url: projects_info[i],
+		title: projects_info[i+1],
+		desc: projects_info[i+2]
+	});
+}
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -14,7 +26,8 @@ app.get('/', function(request, response) {
 	var sub_title="a.k.a Bruce"
   response.render('pages/index', {
   	title: title,
-  	sub_title:sub_title
+  	sub_title:sub_title,
+  	gallery_info: gallery_info
   });
 });
 
